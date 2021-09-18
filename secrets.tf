efe6480136ca52372bec5822b61f998a78fe8dc9
Michael Tharpe <mtharpe@gmail.com>
2021-09-18 18:18:27 -0400

    Comment out test secrents

--- secrets.tf
+++ secrets.tf
@@ -1,21 +1,21 @@
-resource "vault_generic_secret" "jenkins_secret" {
-  path = "kv/jenkins"
+# resource "vault_generic_secret" "jenkins_secret" {
+#   path = "kv/jenkins"
 
-  data_json = <<EOT
-{
-  "username": "jenkins-user",
-  "password": "jenkins-password"
-}
-EOT
-}
+#   data_json = <<EOT
+# {
+#   "username": "jenkins-user",
+#   "password": "jenkins-password"
+# }
+# EOT
+# }
 
-resource "vault_generic_secret" "generic_secret" {
-  path = "kv/generic"
+# resource "vault_generic_secret" "generic_secret" {
+#   path = "kv/generic"
 
-  data_json = <<EOT
-{
-  "username": "generic-user",
-  "password": "generic-password"
-}
-EOT
-}
+#   data_json = <<EOT
+# {
+#   "username": "generic-user",
+#   "password": "generic-password"
+# }
+# EOT
+# }
