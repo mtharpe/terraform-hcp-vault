a3f942d566cf75e7b37ea7e12004a3800a958b18
Michael Tharpe <mtharpe@gmail.com>
2021-09-18 18:33:01 -0400

    Removed dupe

--- secrets.tf
+++ secrets.tf
@@ -1,14 +1,3 @@
-resource "vault_generic_secret" "jenkins_secret" {
-  path = "kv/jenkins"
-
-  data_json = <<EOT
-{
-  "username": "jenkins-user",
-  "password": "jenkins-password"
-}
-EOT
-}
-
 resource "vault_generic_secret" "generic_secret" {
   path = "kv/generic"
 
