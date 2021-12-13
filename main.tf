5e2193a66336c671797e7a3522e2647e48b2691c
Michael Tharpe <mtharpe@hashicorp.com>
2021-12-13 09:57:16 -0500

    Updated flow and version info

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = ">=3.0.0"
+    }
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = ">=0.20.0"
+    }
+  }
+}
+
 provider "vault" {
   address   = data.terraform_remote_state.hcp.outputs.vault_public_address
   token     = hcp_vault_cluster_admin_token.vault_root_token.token
