56544f00b0a11227260fc4ae018c7b84942d6343
Michael Tharpe <mtharpe@hashicorp.com>
2021-12-13 10:23:07 -0500

    Added var descriptions

--- variables.tf
+++ variables.tf
@@ -1,13 +1,30 @@
 ## AWS Vars
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+  description = "AWS Access key"
+}
+variable "aws_secret_key" {
+  description = "AWS Secret key"
+}
 
 ## Azure Vars
-variable "azure_subscription_id" {}
-variable "azure_tenat_id" {}
-variable "azure_client_id" {}
-variable "azure_client_secret" {}
-#variable "azure_environment" {}
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+}
+variable "azure_tenat_id" {
+  description = "Azure Tenat ID"
+}
+variable "azure_client_id" {
+  description = "Azure Client ID"
+}
+variable "azure_client_secret" {
+  description = "Azure Client Secret"
+}
+
+#variable "azure_environment" {
+#description = "Azure Environment"
+#}
 
 ## Userpass Vars
-variable "tharpem_pass" {}
+variable "tharpem_pass" {
+  description = "Michael Tharpes Fake Password"
+}
